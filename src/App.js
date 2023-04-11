c3a55b820f34a56b64bb5e4635ada30f7b3ac75e
AbdulRahman Sharief <111624962+AbdulRahman-Sharief@users.noreply.github.com>
2023-04-11 05:07:43 +0200

    added update employee functionality

--- src/App.js
+++ src/App.js
@@ -7,41 +7,59 @@ function App() {
   const [role, setRole] = useState('dev');
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: 'AbdulRahman',
       role: 'Developer',
       img: 'https://images.pexels.com/photos/1586996/pexels-photo-1586996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
     {
+      id: 2,
       name: 'AbdulRahman',
       role: 'Developer',
       img: 'https://images.pexels.com/photos/1586996/pexels-photo-1586996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
     {
+      id: 3,
       name: 'AbdulRahman',
       role: 'Developer',
       img: 'https://images.pexels.com/photos/1586996/pexels-photo-1586996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
     {
+      id: 4,
       name: 'AbdulRahman',
       role: 'Developer',
       img: 'https://images.pexels.com/photos/1586996/pexels-photo-1586996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
     {
+      id: 5,
       name: 'AbdulRahman',
       role: 'Developer',
       img: 'https://images.pexels.com/photos/1586996/pexels-photo-1586996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
     {
+      id: 6,
       name: 'AbdulRahman',
       role: 'Developer',
       img: 'https://images.pexels.com/photos/1586996/pexels-photo-1586996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
     {
+      id: 7,
       name: 'AbdulRahman',
       role: 'Developer',
       img: 'https://images.pexels.com/photos/1586996/pexels-photo-1586996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
-  ])
+  ]);
+
+  function updateEmployee(id, newName, newRole) {
+    console.log('Update Employee inside of app.js');
+    const updatedEmployees = employees.map((employee) => {
+      if (id === employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  };
   const showEmployees = true;
   return (
     <div className="App">
@@ -56,7 +74,8 @@ function App() {
 
             {employees.map((employee) => {
               return (
-                <Employee key={uuidv4()} name={employee.name} role={employee.role} img={employee.img} />
+                <Employee updateEmployee={updateEmployee}
+                  key={employee.id} id={employee.id} name={employee.name} role={employee.role} img={employee.img} />
               );
             })}
           </div>
